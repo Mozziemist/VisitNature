dba983c41115184a43b9eafc6f27a1b728abf6d5
Marcial Mendoza <MendozaM0717@gmail.com>
2020-12-13 02:01:26 -0800

    geolocation

--- myScript.js
+++ myScript.js
@@ -17,3 +17,45 @@ $(document).ready(function(){
 			return false;
 		});
 });
+
+function geoLocate(dest) {
+  var startPos;
+  var nudge = document.getElementById("nudge");
+
+  var showNudgeBanner = function() {
+    nudge.style.display = "block";
+  };
+
+  var hideNudgeBanner = function() {
+    nudge.style.display = "none";
+  };
+
+  var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
+
+  var geoSuccess = function(position) {
+    hideNudgeBanner();
+    // We have the location, don't display banner
+    clearTimeout(nudgeTimeoutId); 
+
+    // Do magic with location
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+	document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+	
+	var origin = startPos.coords.latitude + "," + startPos.coords.longitude;
+
+  	window.open("https://www.google.com/maps/dir/?api=1&origin=" + origin + "&destination=" + dest);
+  };
+  var geoError = function(error) {
+    switch(error.code) {
+      case error.TIMEOUT:
+        // The user didn't accept the callout
+        showNudgeBanner();
+        break;
+    }
+  };
+
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+
+  
+}
